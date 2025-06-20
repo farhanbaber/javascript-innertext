45f0504d53baaf8d3a1993d8c79e56b54ef54d46
farhan baber <farhanbaber965@gmail.com>
2025-06-20 18:34:56 +0500

    done

---
+++ script.js
@@ -0,0 +1,14 @@
+let quesOne = document.getElementById("quesOne")
+function Ans() {
+    quesOne.innerText = "Walaikumaslam"
+}
+
+let quesTwo = document.getElementById("quesTwo")
+function AnsTwo() {
+    quesTwo.innerText = "I am fine Thanks for Asking"
+}
+let quesThree = document.getElementById("quesThree")
+function AnsThree() {
+    quesThree.innerText = "My Name is Eng : Farhan baber    "
+    
+}
